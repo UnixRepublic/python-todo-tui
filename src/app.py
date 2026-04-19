2ee9d9ef88c442769f99bb0128ecfb71e3ba2831
Unix Republic <unixrepublic@proton.me>
2026-04-19 19:26:18 +0100

    Added graceful exiting

--- src/app.py
+++ src/app.py
@@ -1,9 +1,21 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Label, Checkbox, Digits
+from textual.screen import ModalScreen
+from textual.widgets import Label, Checkbox, Digits, Footer
 from textual.containers import HorizontalGroup, VerticalScroll
 
 class CountDown(Digits):
     """ A widget to display time remaining until deadline """
+
+class CreateNewTaskScreen(ModalScreen):
+    """ Screen with a form to create a new task """
+
+    BINDINGS = [("Q", "quit", "Quit")]
+
+    def compose(self) -> ComposeResult:
+        yield Label("TO DO IRONICALLY")
+
+    def action_quit(self) -> None:
+        self.app.pop_screen()
 
 class Task(HorizontalGroup):
     """ A task to-do """
@@ -14,12 +26,23 @@ class Task(HorizontalGroup):
         yield Label("Test Title for TODO")
         yield CountDown("00:00:00")
 
-
 class TODOTUI(App):
     """ A textual app to manage tasks"""
+
+    BINDINGS = [("Q", "quit", "Quit"), ("N", "create_new_task", "Create new task")]
+
     def compose(self) -> ComposeResult:
         """ Create child widgets for the app """
+        yield Footer()
         yield VerticalScroll(Task(), Task(), Task())
+
+    def action_quit(self) -> None:
+        """ An action to quit the app """
+        self.exit(0)
+
+    def action_create_new_task(self) -> None:
+        self.push_screen(CreateNewTaskScreen())
+
 
 if __name__ == "__main__":
     app = TODOTUI()
